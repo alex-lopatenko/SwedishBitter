20c6112d39dbdf97b1e8c20243222e3731a94655
Alexey <alexey-lopatenko@yandex.ru>
2022-05-05 20:05:29 +0300

    Add block Top, List

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 $(function(){
-
-  $('.list__inner .tab').on('click' ,function(event) {
+  $('.list__inner .tab').on('click', function(event) {
     var id = $(this).attr('data-id');
       $('.list__inner').find('.tab-item').removeClass('active-tab').hide();
       $('.list__inner .tabs').find('.tab').removeClass('active');
@@ -8,5 +7,4 @@ $(function(){
       $('#'+id).addClass('active-tab').fadeIn();
       return false;
     });
-
 });
